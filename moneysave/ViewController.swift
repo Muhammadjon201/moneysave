945bee467be6c1620b994113cce92519677260db
Muhammadjon <mamarasulov911@gmail.com>
2023-05-23 23:54:42 +0900

    Initial Commit

---
+++ moneysave/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moneysave
+//
+//  Created by ericzero on 5/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
