27cbc7e34410d881f4f441d1fa7aeef4cc07b0bd
Muhammadjon <mamarasulov911@gmail.com>
2023-05-25 05:23:50 +0900

    - adds textField to sighUp page

--- moneysave/Auth/SignUpViewController.swift
+++ moneysave/Auth/SignUpViewController.swift
@@ -11,8 +11,24 @@ final class SignUpViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "SignUp updated"
-        
+        view.backgroundColor = .white
+        title = "Sign Up"
+        setConstraints()
     }
     
+}
+
+extension SignUpViewController {
+    
+    func setConstraints(){
+        let nameField = TextfieldFactory.createTextField(placeholder: "Name", color: UIColor.systemGray3)
+        
+        view.addSubview(nameField)
+        nameField.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(50)
+            make.left.equalTo(15)
+            make.right.equalTo(-15)
+            make.height.equalTo(56)
+        }
+    }
 }
