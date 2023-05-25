cd2ce2ed39a01168194197cdd78c8014f134c243
Muhammadjon <mamarasulov911@gmail.com>
2023-05-25 22:00:28 +0900

    - implemented passwordField to signupPage

--- moneysave/Auth/SignUpViewController.swift
+++ moneysave/Auth/SignUpViewController.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 final class SignUpViewController: UIViewController {
+    
+    private var isChecked = false
+    
+    lazy var tickBtn: UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(named: "checkNo"), for: .normal)
+        btn.addTarget(self, action: #selector(tickBtnTapped), for: .touchUpInside)
+        return btn
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,14 +30,49 @@ final class SignUpViewController: UIViewController {
 extension SignUpViewController {
     
     func setConstraints(){
-        let nameField = TextfieldFactory.createTextField(placeholder: "Name", color: UIColor.systemGray3)
+        let nameField = TextfieldFactory.createTextField(placeholder: "Name", color: UIColor.systemGray5)
+        let emailField = TextfieldFactory.createTextField(placeholder: "Email", color: UIColor.systemGray5)
+        let passwordField = TextfieldFactory.createTextField(placeholder: "Password", color: UIColor.systemGray5)
         
         view.addSubview(nameField)
+        view.addSubview(emailField)
+        view.addSubview(passwordField)
+        view.addSubview(tickBtn)
+        
         nameField.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(50)
             make.left.equalTo(15)
             make.right.equalTo(-15)
             make.height.equalTo(56)
+        }
+        
+        emailField.snp.makeConstraints { make in
+            make.top.equalTo(nameField.snp.bottom).offset(24)
+            make.left.equalTo(15)
+            make.right.equalTo(-15)
+            make.height.equalTo(56)
+        }
+        
+        passwordField.snp.makeConstraints { make in
+            make.top.equalTo(emailField.snp.bottom).offset(24)
+            make.left.equalTo(15)
+            make.right.equalTo(-15)
+            make.height.equalTo(56)
+        }
+        
+        tickBtn.snp.makeConstraints { make in
+            make.top.equalTo(passwordField.snp.bottom).offset(15)
+            make.left.equalTo(15)
+            make.height.width.equalTo(32)
+        }
+    }
+    
+    @objc func tickBtnTapped(){
+        isChecked.toggle()
+        if isChecked {
+            tickBtn.setImage(UIImage(named: "checkYes"), for: .normal)
+        } else {
+            tickBtn.setImage(UIImage(named: "checkNo"), for: .normal)
         }
     }
 }
